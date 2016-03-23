881b808499db2fd8c65e1158439561b76c9572b7
aBontia <abontia@imanet.org>
2016-03-23 04:42:00 +0000

    Commiting Ch-12 Assignment

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,24 @@
 var myName = "Alex", 
     courseTrack = "11", 
     courseType = "BLOC Frontend Development";
+
+//CHAPTER 12 ASSIGNMENT:
+//2a.
+console.log(3+5);
+console.log(12-5);
+//2b.
+console.log("I\'m enrolled in a course called \"Bloc Frontend Web Development\".\n I\'ve already learned some HTML and CSS, and now I\'m learning JavaScript.\n By the end, I will have learned how to build a complete frontend application.");
+
+//2c.
+console.log(5 < 6); 
+console.log(5 > 6); 
+
+console.log("5" == myName);
+console.log(11 == 21);
+console.log("eleven" == 11); 
+
+console.log(myName == "Alex");
+console.log(myName === "Alex");
+console.log(myName === "Alex" &&  courseTrack === "javascript");
+console.log(courseType && "Alex");
+console.log(5 === "5");
